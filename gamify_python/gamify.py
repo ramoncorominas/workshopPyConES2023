a03042ad53e13d740542ef73e8e8de67954c7bba
Ramón Corominas <ramon@ramoncorominas.com>
2023-10-05 18:42:57 +0200

    refactor: modify the way of importing the game so it is always read from the file (even when packed)

--- gamify_python/gamify.py
+++ gamify_python/gamify.py
@@ -1,5 +1,20 @@
 from ui import show_message
-from games.guess_game import play_game
+
+# we need to change the way we import the external game module to ensure
+# the `guess_game.py` file is imported from outside the packed executable
+# from games.guess_game import play_game
+import os
+import sys
+import importlib
+
+try:
+    start_file = __file__  # unpacked
+except NameError:  # __file__ not defined
+    start_file = sys.executable  # packed into .exe
+app_dir = os.path.dirname(start_file)
+sys.path.insert(0, app_dir)
+play_game = importlib.import_module('games.guess_game').play_game
+
 
 if __name__ == '__main__':
     show_message('¡Hola! ¡Vamos a jugar!')
