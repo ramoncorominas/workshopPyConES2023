f93190fe9221cd221454b99aa5b545f9ada059e4
Ramón Corominas <ramon@ramoncorominas.com>
2023-10-05 04:21:51 +0200

    debug: modify adapter interface and debug configuration to allow true remote debugging

--- gamify_python/gamify.py
+++ gamify_python/gamify.py
@@ -6,7 +6,7 @@ import debugpy
 if not debugpy.is_client_connected():
     this_dir = os.path.dirname(__file__)
     debugpy.log_to(os.path.join(this_dir, 'debug_logs'))  # optional
-    debugpy.listen(5678)
+    debugpy.listen(('0.0.0.0', 5678))  # interface accepting connections
     print('Esperando por el cliente de depuración...')
     debugpy.wait_for_client()  # optional
 
